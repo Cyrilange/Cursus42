2f42f2c2c531fae09dd153da76742069e6830d61
Cyrilange <cyril.r.a.salamite@gmail.com>
2024-10-31 17:28:20 +0100

    31/10

---
+++ exam_rank_02/level_4/flood_fill/flood_fill.h
@@ -0,0 +1,13 @@
+#ifndef FLOOD_FILL
+#define FLOOD_FILL
+
+#include <stdio.h>
+#include <stdlib.h>
+
+  typedef struct  s_point
+  {
+    int           x;
+    int           y;
+  }               t_point;
+
+#endif
