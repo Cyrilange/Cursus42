b3fbbfcdf73dc3ca8e73510dfb84596b8a9d8ea2
Cyrilange <cyril.r.a.salamite@gmail.com>
2025-01-07 14:02:47 +0100

    exam

--- exam2/llevelThree/tab_mult.c
+++ exam2/llevelThree/tab_mult.c
@@ -5,10 +5,6 @@ The parameter will always be a strictly positive number that fits in an int,
 and said number times 9 will also fit in an int.
 
 If there are no parameters, the program displays \n.
- Escribe un programa que muestre la tabla de multiplicar de un número.
- El parámetro siempre será un número estrictamente positivo que cabe en un int,
-     y dicho número multiplicado por 9 también encajará en un int.
-   Si no hay parámetros, el programa muestra \n.
 
 Examples:
 
@@ -36,3 +32,49 @@ $>
 $>./tab_mult | cat -e
 $
 $>*/
+
+#include <unistd.h>
+
+int ft_atoi(char *str)
+{
+	int i = 0;
+	int result = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = (result * 10) + str[i] - '0';
+		i++;
+	}
+	return result;
+} 
+
+void ft_putnbr(int n)
+{
+	char c;
+	if (n > 9)
+		ft_putnbr(n / 10);
+	c = n % 10 + '0';
+	write(1, &c, 1);
+}
+
+int main(int argc, char **argv)
+{
+	int i = 1;
+	if (argc < 2)
+		write(1, "\n", 1);
+	else
+	{
+		int num = ft_atoi(argv[1]);
+		while (i >= 1 && i <= 9)
+		{
+			ft_putnbr(i);
+			write(1, " x ", 3);
+			ft_putnbr(num);
+			write(1, " = ", 3);
+			ft_putnbr(i * num);
+			write(1, "\n", 1);
+			
+			i++;
+		}
+	}
+	return 0;
+}
