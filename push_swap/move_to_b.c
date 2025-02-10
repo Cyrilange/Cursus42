98de894d01c0571aebd95b1e8e0621148a22ffd1
Cyrilange <cyril.r.a.salamite@gmail.com>
2025-02-10 21:47:16 +0100

    a

--- push_swap/move_to_b.c
+++ push_swap/move_to_b.c
@@ -42,22 +42,21 @@ static void	set_target_a(t_stack_node *a, t_stack_node *b)
 	while (a)
 	{
 		best_match_index = LONG_MIN;
-                current_b = b;
-                while (current_b)
-                {
-					if (current_b->number < a->number 
-						&& current_b->number > best_match_index)
-					{
-						best_match_index = current_b->number;
-						target_node = current_b;
-					}
-					current_b = current_b->next;
-                }
-				if (best_match_index == LONG_MIN)
-					a->target_node = find_max_node(b);
-				else
-					a->target_node = target_node;
-				a = a->next;
+		current_b = b;
+		while (current_b)
+		{
+			if (current_b->number < a->number && current_b->number > best_match_index)
+			{
+				best_match_index = current_b->number;
+				target_node = current_b;
+			}
+			current_b = current_b->next;
+		}
+		if (best_match_index == LONG_MIN)
+			a->target_node = find_max_node(b);
+		else
+			a->target_node = target_node;
+		a = a->next;
 	}
 }
 
