d26b9621aec3fbd019186255ce00bd5c768b719c
Your Name <csalamit@c3r5s6.42malaga.com>
2025-09-06 18:49:02 +0200

    add the prompt

--- c++_module0/ex01/phonebook.cpp
+++ c++_module0/ex01/phonebook.cpp
@@ -12,12 +12,10 @@
 
 #include "phonebook.hpp"
 
-
-
-int main(void) {
+std::string prompt_enter() {
 	std::string prompt;
 	while (true) {
-		std::cout << "Choose an option between ADD SEARCH EXIT : ";
+		std::cout << "Choose between ADD SEARCH EXIT : ";
 		std::getline(std::cin, prompt);
 		if (prompt == "ADD" || prompt == "SEARCH" || prompt == "EXIT") {
 			std::cout << "your choose : " << prompt << std::endl;
@@ -27,6 +25,11 @@ int main(void) {
 		std::cout << "Invalid option : " << std::endl;
 		}
 	}
+	return (prompt);
+}
+
+int main(void) {
+	std::string input = prompt_enter();
 
 	
 	return 0;
