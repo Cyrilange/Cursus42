543f53418409a7e41ea9f6a759474fe59189d38c
Cyrilange <cyril.r.a.salamite@gmail.com>
2025-09-15 21:15:14 +0200

    finish

--- C++/module02/ex00/Fixed.cpp
+++ C++/module02/ex00/Fixed.cpp
@@ -8,12 +8,21 @@ Fixed::Fixed(const Fixed& copy) :value(copy.value) {
     std::cout << "Copy constructor called" <<std::endl;
 };
 
+int Fixed::getRawBits( void ) const {
+    std::cout << "getRawBits member function called" << std::endl;
+    return this->value;
+};
+
 Fixed& Fixed::operator=(const Fixed& copy) {
     std::cout << "Copy assignment operator called " << std::endl;
     if (this != &copy) {
         this->value = copy.value;
     }
     return *this;
+};
+
+void Fixed::setRawBits( int const raw ) {
+ this->value = raw;
 };
 
 Fixed::~Fixed() {
--- C++/module02/ex00/Fixed.hpp
+++ C++/module02/ex00/Fixed.hpp
@@ -7,13 +7,17 @@ class Fixed {
 
     private:
         int value; //variable to store the fixed point
-        static const int eight = 8;
+        static const int eight = 8; //use like fixed_value = float_value * (1 << eight)
 
     public:
         Fixed();
         Fixed(const Fixed& copy);
         Fixed& operator=(const Fixed& copy);
         
+        
+        int getRawBits( void ) const; //returns the raw value of the fixed-point value
+        void setRawBits( int const raw ); //  sets the raw value of the fixed-point number.
+
         static int getEight() { return eight; } 
         ~Fixed();
 
