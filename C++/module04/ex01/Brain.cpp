c7eec788dd8c692c02419b780706f618732cfd34
Cyrilange <cyril.r.a.salamite@gmail.com>
2025-10-01 15:41:22 +0200

    cpp4-ex01

---
+++ C++/module04/ex01/Brain.cpp
@@ -0,0 +1,25 @@
+#include "Brain.hpp"
+
+Brain::Brain() {
+    std::cout << "Brain default constructor" << std::endl;
+}
+
+Brain::Brain(const Brain& other)  {
+
+}
+
+Brain& Brain::operator=(const Brain& other) {
+
+}
+
+const std::string& Brain::getIdea(unsigned int i) const {
+
+}
+
+void Brain::setIdea(unsigned int i, std::string idea) {
+
+}
+
+~Brain() {
+
+}
--- C++/module04/ex01/Brain.hpp
+++ C++/module04/ex01/Brain.hpp
@@ -6,7 +6,13 @@
 #include <string>
 
 class Brain {
-
+    public:
+        Brain();
+        Brain(const Brain& other);
+        Brain& operator=(const Brain& other);
+        const std::string& getIdea(unsigned int i) const;
+        void setIdea(unsigned int i, std::string idea);
+        ~Brain();
     private:
         std::string ideas[100];
 };
