bb327a9efcc37b502959841fd6974136246d8702
Cyrilange <cyril.r.a.salamite@gmail.com>
2025-10-04 22:22:58 +0200

    fix

--- C++/module01/ex06/Harl.cpp
+++ C++/module01/ex06/Harl.cpp
@@ -14,28 +14,48 @@
 
 
 
-Harl::Harl() {}
+Harl::Harl() {};
+
+void Harl::debug(void) {
+    std::cout << "< DEBUG >" << std::endl;
+    std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl << std::endl;
+}
+
+void Harl::info(void) {
+    std::cout << "< INFO >" << std::endl;
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl << std::endl;
+}
+
+void Harl::warning(void) {
+    std::cout << "< WARNING >" << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years, whereas you started working here just last month." << std::endl << std::endl;
+}
+
+void Harl::error(void) {
+    std::cout << "< ERROR >" << std::endl;
+    std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl << std::endl;
+}
+
+
 
 
 void Harl::callDebug(void) {
-    void (Harl::*ptr)() = &Harl::debug;
-    (this->*ptr)();
+    debug();
 }
 
 void Harl::callInfo(void) {
-    void (Harl::*ptr)() = &Harl::info;
-    (this->*ptr)();  
+    info();
 }
 
 void Harl::callWarning(void) {
-    void (Harl::*ptr)() = &Harl::warning;
-    (this->*ptr)();
+    warning();
 }
 
 void Harl::callError(void) {
-    void (Harl::*ptr)() = &Harl::error;
-    (this->*ptr)(); 
+    error();
 }
+
+
 
 int Harl::getLevel(const std::string& level) {
     return static_cast<int>(stringToLevel(level));
--- C++/module01/ex06/main.cpp
+++ C++/module01/ex06/main.cpp
@@ -17,7 +17,7 @@
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        std::cerr << "must use with an arg" << std::endl;
+        std::cerr << "must be used with an arg" << std::endl;
         return -1;
     }
     Harl harl;
@@ -26,9 +26,10 @@ int main(int argc, char *argv[]) {
     if (input == "DEBUG" || input == "INFO" || input == "WARNING" || input == "ERROR") {
         
         harl.complain(input);
+        
     }
-    else if (input == "I do not like this")
-        std::cout << "INFO" << std::endl;
+    std::cout << "\n";
+    std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 
     return 0;
 }
