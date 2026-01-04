3175307edda5fb0e2221f4fd1bdd713b44af6520
Cyrilange <cyril.r.a.salamite@gmail.com>
2026-01-04 18:07:58 +0100

    test

--- C++/module05/ex02/main.cpp
+++ C++/module05/ex02/main.cpp
@@ -6,15 +6,8 @@
 #include <ctime>
 #include <iostream>
 
-#define RESET   "\033[0m"
-#define RED     "\033[31m"
-#define GREEN   "\033[32m"
-#define YELLOW  "\033[33m"
-#define BLUE    "\033[34m"
-#define MAGENTA "\033[35m"
-#define CYAN    "\033[36m"
-
 int main() {
+	std::system("clear");
     std::srand(std::time(NULL));
 
     std::cout << CYAN << "==============================================" << RESET << std::endl;
